b4e00b7114877fd58fd77422b602e94d8b348e59
sunhokim1457 <sunhokim.public@gmail.com>
2023-08-26 01:27:38 +0900

    refactor : crawling Dag CSV파일 > Parquet 파일로 수정

--- airflow/dags/news_article_crawling_DAG.py
+++ airflow/dags/news_article_crawling_DAG.py
@@ -9,6 +9,8 @@ import datetime
 import pendulum
 import csv
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
@@ -49,8 +51,8 @@ with DAG(
         logging.info(f"-- logical_date : {logical_date_kst} --\n-- 해당 날짜에서 뉴스 데이터를 가져옵니다. --")
 
         naver_news_csv_filename = "data/naver_news/naver_news_" + str(logical_date_kst.date()) + ".csv"
-        news_article_csv_filename = "data/news_article/news_article_" + str(logical_date_kst.date()) + ".csv"
-        kwargs['ti'].xcom_push(key='news_article_csv_filename', value=news_article_csv_filename)
+        news_article_parquet_filename = "data/news_article/news_article_" + str(logical_date_kst.date()) + ".parquet"
+        kwargs['ti'].xcom_push(key='news_article_parquet_filename', value=news_article_parquet_filename)
 
         with open(naver_news_csv_filename, 'r', encoding='utf-8') as f:
             csv_reader = csv.reader(f)
@@ -79,22 +81,25 @@ with DAG(
                 logging.info(f"{len(total_news_article_list)} : {corpname}의 뉴스 {line[2]} 저장")
             
             df = pd.DataFrame(total_news_article_list)
-            df.to_csv(news_article_csv_filename, index=False, encoding='utf-8')
+            df['pubdate'] = pd.to_datetime(df['pubdate'])
+            table = pa.Table.from_pandas(df)
+            pq.write_table(table, news_article_parquet_filename)
+            # df.to_csv(news_article_csv_filename, index=False, encoding='utf-8')
 
-    # s3에 csv 파일 업로드
-    def upload_csv_to_s3(**kwargs):
+    # s3에 parquet 파일 업로드
+    def upload_parquet_to_s3(**kwargs):
         s3_hook = S3Hook(aws_conn_id='S3_conn')
         logging.info("S3 Connection Success")
 
-        news_article_csv_filename = kwargs['ti'].xcom_pull(task_ids='get_news_article_crawling_csv_task', key='news_article_csv_filename')
+        news_article_parquet_filename = kwargs['ti'].xcom_pull(task_ids='get_news_article_crawling_parquet_task', key='news_article_parquet_filename')
         
         s3_hook.load_file(
-            filename=news_article_csv_filename, 
-            key=news_article_csv_filename,
+            filename=news_article_parquet_filename, 
+            key=news_article_parquet_filename,
             bucket_name="de-4-3",
             replace=True
             )
-        logging.info("Upload CSV File Success")
+        logging.info("Upload Parquet File Success")
 
     # 네이버 뉴스 크롤링
     def news_crawling_from_link(link):
@@ -234,23 +239,23 @@ with DAG(
         except Exception as e:
             logging.error(f"기타 에러 / 링크 : {link} / 에러 : {e}")
 
-        article = article.replace('\n', '').strip()    
+        article = article.replace('\n', '').replace('\r', '').replace('\t', '').strip()    
         return article
 
 
     # Task
 
 
-    get_news_article_crawling_csv_task = PythonOperator(
-        task_id='get_news_article_crawling_csv_task',
+    get_news_article_crawling_parquet_task = PythonOperator(
+        task_id='get_news_article_crawling_parquet_task',
         python_callable= get_news_article_crawling_data_from_naver_news,
         provide_context=True,
         dag=dag
     )
 
-    upload_news_article_csv_to_s3_task = PythonOperator(
-        task_id='upload_news_article_csv_to_s3_task',
-        python_callable= upload_csv_to_s3,
+    upload_news_article_parquet_to_s3_task = PythonOperator(
+        task_id='upload_news_article_parquet_to_s3_task',
+        python_callable= upload_parquet_to_s3,
         provide_context=True,
         dag=dag
     )
@@ -258,10 +263,10 @@ with DAG(
     news_article_crawling_s3_to_redshift_task = S3ToRedshiftOperator(
         task_id = 'news_article_crawling_s3_to_redshift_task',
         s3_bucket = "de-4-3",
-        s3_key = "{{ ti.xcom_pull(task_ids='get_news_article_crawling_csv_task', key='news_article_csv_filename') }}",
+        s3_key = "{{ ti.xcom_pull(task_ids='get_news_article_crawling_parquet_task', key='news_article_parquet_filename') }}",
         schema = "raw_data",
         table = "news_article",
-        copy_options=["csv", "IGNOREHEADER 1"],
+        copy_options=['parquet'],
         redshift_conn_id = "redshift_conn",
         aws_conn_id = "S3_conn",    
 
@@ -270,4 +275,4 @@ with DAG(
         dag = dag
     )
 
-    get_news_article_crawling_csv_task >> upload_news_article_csv_to_s3_task >> news_article_crawling_s3_to_redshift_task
+    get_news_article_crawling_parquet_task >> upload_news_article_parquet_to_s3_task >> news_article_crawling_s3_to_redshift_task
