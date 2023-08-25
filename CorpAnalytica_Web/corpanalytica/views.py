b8e19a00a569a50a2eabfa84dfe757dbce99c833
sunhoKim <sunhokim.public@gmail.com>
2023-08-25 16:36:13 +0900

    feat : Django 웹 초기 구성

--- CorpAnalytica_Web/corpanalytica/views.py
+++ CorpAnalytica_Web/corpanalytica/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello, world.")
+def home_view(request):
+    return render(request,'home.html')
+
+def search_view(request):
+    if request.method == 'POST':
+            searched = request.POST['searched']        
+            # recipes = Recipe.objects.filter(name__contains=searched) # Recipe는 임시 모델의 클래스 이름
+            # return render(request, 'searched.html', {'searched': searched, 'recipes': recipes})
+            return render(request, 'search.html', {'searched': searched})
+    else:
+            return render(request, 'search.html', {})
+
+def corp_detail_view(request):
+    return render(request,'corp_detail.html')
