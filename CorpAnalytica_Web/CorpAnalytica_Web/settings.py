76a55028b73d0936fca2aa76480cf177d3b810c8
seungeon-kim <tmddjs112740@gmail.com>
2023-08-31 22:16:55 +0900

    nginx test

--- CorpAnalytica_Web/CorpAnalytica_Web/settings.py
+++ CorpAnalytica_Web/CorpAnalytica_Web/settings.py
@@ -146,6 +146,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join('static/'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
